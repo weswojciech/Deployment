a7f40ff90b12f7f1c3171b32fca3ce91fd4c79a7
Wojciech <weswojciech@gmail.com>
2024-06-14 08:36:31 +0200

    first commit

--- src/app.js
+++ src/app.js
@@ -1,20 +1,20 @@
 const express = require('express');
 const { userRouter } = require('./rourters/user.router.js');
+const dotenv = require('dotenv');
 
-
+dotenv.config();
 const app = express();
-const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+	res.send('Hello World!');
 });
 
 app.use('/users', userRouter);
 
-app.listen(PORT, (err) => {
-  if (!err) {
-    console.log('server running on ', PORT);
-  } else {
-    console.log("Error")
-  }
-})
+app.listen(process.env.PORT, err => {
+	if (!err) {
+		console.log('server running on ', process.env.PORT);
+	} else {
+		console.log('Error');
+	}
+});
